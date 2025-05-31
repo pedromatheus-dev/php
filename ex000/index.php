cf3a33cb78f00178b591fe6c2e22753d03db57c3
Pedro Matheus <46729924+pedromatheus-dev@users.noreply.github.com>
2025-05-30 22:33:42 -0300

    criei ex000

---
+++ ex000/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Olá Mundo</title>
+</head>
+
+<body>
+    <h1>
+        <?php 
+            echo "Olá mundo! \u{1f30d}";
+        ?>
+    </h1>
+    <p>Me livrei da maldição</p>
+</body>
+
+</html>
