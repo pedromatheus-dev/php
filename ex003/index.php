6fa71c785dbe0a516126c3a15e8161b37bac8bc1
Pedro Matheus <46729924+pedromatheus-dev@users.noreply.github.com>
2025-06-01 21:34:55 -0300

    criei ex003

---
+++ ex003/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+</head>
+
+<body>
+    <h1>Variáveis</h1>
+    <?php
+        $nome = "Matheus";
+        $idade = 20;
+        const PAIS = "Brasil";
+        echo "<p> Meu nome é $nome, tenho $idade anos e moro no " . PAIS ."</p>";
+        
+    ?>
+</body>
+
+</html>
