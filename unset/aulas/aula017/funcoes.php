e8ee2c8a1786c66eed0fca904765bbd61f8314f0
Pedro Matheus <46729924+pedromatheus-dev@users.noreply.github.com>
2025-06-16 22:21:28 -0300

    criei aula017

---
+++ unset/aulas/aula017/funcoes.php
@@ -0,0 +1,6 @@
+<?php 
+
+    function imprimirMensagem(string $msg) {
+        echo "<p>$msg</p>";
+    }
+?>
---
+++ unset/aulas/aula017/index.php
@@ -0,0 +1,13 @@
+<?php 
+    declare(strict_types=1); // Declaração de tipos estritos, onde os tipos de dados devem ser exatamente os especificados.
+
+    include 'funcoes.php'; // Inclui o arquivo de configuração, que contém a função carregarConfiguracao.
+
+    $texto = "testando";
+
+    imprimirMensagem($texto); // Chama a função imprimirMensagem passando a variável $texto como argumento.
+
+    $numero = 10;
+
+    imprimirMensagem($numero); // Erro, pois a função espera uma string, mas foi passado um inteiro.
+?>
