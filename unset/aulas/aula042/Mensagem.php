2c001ecfe7a44268937ef299652c865d506f748c
Pedro Matheus <46729924+pedromatheus-dev@users.noreply.github.com>
2025-06-25 10:49:19 -0300

    criei aula042

---
+++ unset/aulas/aula042/Mensagem.php
@@ -0,0 +1,40 @@
+<?php 
+
+class Mensagem{
+    private $texto;
+    private $css;
+
+    public function renderizar() : string {
+        return "<div class='{$this->css}'>{$this->texto}</div>";
+    }
+
+    private function filtrar(string $msg) : string {
+        return filter_var($msg, FILTER_SANITIZE_SPECIAL_CHARS);
+    }
+
+    public function sucesso(string $msg) : string {
+        
+        $this->css = "alert alert-success";
+        $this->texto = $this->filtrar($msg);
+
+        return $this->renderizar();
+    }
+    
+    public function erro(string $msg) : string {
+        
+        $this->css = "alert alert-danger";
+        $this->texto = $this->filtrar($msg);
+
+        return $this->renderizar();
+    }
+
+    public function info(string $msg) : string {
+        
+        $this->css = "alert alert-primary";
+        $this->texto = $this->filtrar($msg);
+
+        return $this->renderizar();
+    }
+}
+
+?>
---
+++ unset/aulas/aula042/index.php
@@ -0,0 +1,17 @@
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet">
+
+<?php 
+
+    include './Mensagem.php';
+
+    $msg = new Mensagem();
+
+    $conteudoSucesso = "Isso é uma mensagem de sucesso";
+    $conteudoErro = "Isso é uma mensagem de erro";
+    $conteudoInfo = "Isso é uma mensagem de informação";
+
+    echo $msg->sucesso($conteudoSucesso);
+    echo $msg->erro($conteudoErro);
+    echo $msg->info($conteudoInfo);
+
+?>
