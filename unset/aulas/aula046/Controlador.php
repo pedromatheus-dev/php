ff7513381df59f134b86fe64530271973245b07f
Pedro Matheus <46729924+pedromatheus-dev@users.noreply.github.com>
2025-06-28 17:19:56 -0300

    criei aula046

---
+++ unset/aulas/aula046/Controlador.php
@@ -0,0 +1,11 @@
+<?php 
+    namespace aula046;
+
+    class Controlador{
+        public function __construct(string $texto)
+        {   
+            echo $texto;
+        }
+    }
+
+?>
---
+++ unset/aulas/aula046/index.php
@@ -0,0 +1,12 @@
+<?php 
+
+    include "./Controlador.php";
+
+    use aula046\Controlador;
+
+    $ctrl1 = new Controlador("Controlador 1");
+
+    echo "<hr>";
+
+    $ctrl2 = new Controlador("Controlador 2");
+?>
